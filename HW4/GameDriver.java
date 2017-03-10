8ab5c3e24529ffcb32dc1fd98bd671011fc063ea
Hung V Le <leviethung041_140@yahoo.com>
2017-03-09 21:11:59 -0800

    Add files via upload

---
+++ HW4/GameDriver.java
@@ -0,0 +1,32 @@
+import Game.Game;
+import Game.ProxyGame;
+
+/* Version History  :
+ Class   : GameDriver
+ Author     : Karthick
+              Hung Le
+              Heli
+              Janani
+ Description: The test/run driver of the program 
+ --===========================================================================================================================
+ -- Rev      Date            By                                                        Description                                                 
+ --===========================================================================================================================
+ -- 1.0    20-Feb-2017     Hung Le                     1. Main driver of the program
+ --===========================================================================================================================
+ --============================================================================================================================*/
+
+public class GameDriver {
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+
+		Game game = null;
+		ProxyGame proxy = new ProxyGame (game);
+		proxy.option();
+	}
+
+}
